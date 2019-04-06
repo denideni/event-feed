a36755e5e309bf24de995183223a9dffb1fd721b
Deniz <kecik.deniz@gmail.com>
2019-04-06 15:54:42 -0400

    initial commit

---
+++ public/javascripts/app.js
@@ -0,0 +1,230 @@
+var app = angular.module('angularjsNodejsTutorial', []);
+app.controller('loginController', function ($scope, $http) {
+  console.log("HERE in login");
+  $scope.verifyLogin = function () {
+    console.log('clicked')
+    // To check in the console if the variables are correctly storing the input:
+    // console.log($scope.username, $scope.password);
+    // console.log($scope);
+    var request = $http({
+      url: '/login',
+      method: "POST",
+      data: {
+        'username': $scope.username,
+        'password': $scope.password
+      }
+    })
+
+    request.success(function (response) {
+      console.log(response);
+      if (response.status === "OK") {
+        window.location.href = "http://localhost:8081/dashboard"
+      }
+    });
+    request.error(function (err) {
+      // failed
+      console.log("error: ", err);
+    });
+  };
+});
+
+app.controller('registerController', function($scope, $http) {
+  console.log('HERE in registercontroller')
+  $scope.registerUser = function() {
+    var request = $http({
+      url: '/register',
+      method: 'POST',
+      data: {
+        'username': $scope.username,
+        'name': $scope.name,
+        'password': $scope.password
+      }
+    })
+    request.success(function (response) {
+      console.log(response);
+      if (response.status === "OK") {
+        window.location.href = "http://localhost:8081/dashboard"
+      }
+    });
+    request.error(function (err) {
+      // failed
+      console.log("error: ", err);
+    });
+  }
+})
+
+// app.controller('getUserController', function ($scope, $http) {
+//   // Angular function
+//   console.log("HERE in controller");
+//   $scope.getUsers = function () {
+//     var request = $http({
+//       url: '/dashboardUsers',
+//       method: 'GET'
+//     })
+
+//     request.success(function (response) {
+//       // success
+//       $scope.users = response.rows;
+//     });
+//     request.error(function (err) {
+//       // failed
+//       console.log("error: ", err);
+//     });
+//   };
+// });
+
+// app.controller('getGenresController', function ($scope, $http) {
+//   $scope.getGenres = function () {
+//     var request = $http({
+//       url: '/genres',
+//       method: 'GET'
+//     })
+
+//     request.success(function (response) {
+//       $scope.genres = response.rows;
+//     });
+//     request.error(function (err) {
+//       console.log('error: ' + err);
+//     });
+//   }
+// })
+
+// app.controller('getGenreTopMoviesController', function ($scope, $http) {
+//   console.log('HERE IN GENRES TOP CONTROLLER')
+//   $scope.getMoviesUnderGenre = function (curr_genre) {
+//     var request = $http({
+//       url: '/genreTop10',
+//       method: 'POST',
+//       data: {
+//         'currentGenre': curr_genre
+//       }
+//     })
+
+//     request.success(function (response) {
+//       $scope.top10Movies = response.rows;
+//     });
+//     request.error(function (err) {
+//       console.log('error: ' + err);
+//     })
+//   }
+// })
+
+// app.controller('getRecommendationsController', function ($scope, $http) {
+
+//   $scope.submitChoice = function (curr_movie_id) {
+//     console.log(curr_movie_id)
+//     console.log('clicked')
+//     var request = $http({
+//       url: '/recommendationsurl',
+//       method: "POST",
+//       data: {
+//         'current_choice': curr_movie_id
+//       }
+//     })
+
+//     request.success(function (response) {
+//       // success
+//       console.log(response.rows);
+//       $scope.resultMovies = response.rows;
+//       if (response.result === "success") {
+//         // After you've written the INSERT query in routes/index.js, uncomment the following line
+//       }
+//     });
+//     request.error(function (err) {
+//       // failed
+//       console.log("error: ", err);
+//     });
+//   };
+// });
+
+// app.controller('yearOptionsController', function ($scope, $http) {
+//   console.log("inside yearoptionscontroller")
+//   $scope.options = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
+//   $scope.chooseYear = function (curr_year) {
+//     var request = $http({
+//       url: '/yearsandyears',
+//       method: "POST",
+//       data: {
+//         'current_year': curr_year
+//       }
+//     })
+//     request.success(function (response) {
+//       // success
+//       $scope.movies = response.rows;
+//       console.log(response.rows);
+//     });
+//     request.error(function (err) {
+//       // failed
+//       console.log("error: ", err);
+//     });
+//   }
+// })
+
+// app.controller('postersController', function($scope, $http) {
+//   console.log("inside posters controller");
+//   $scope.getPosters = function() {
+//     var request = $http({
+//       url: "/randomIMDBIds",
+//       //url: "http://img.omdbapi.com/?i=tt3896198&apikey=d0656bab",
+//       //url: 'http://www.omdbapi.com/?t=' + 'tt3896198' + '&apikey=d0656bab', //add +'&apikey=YOUR_API_KEY'',
+//       method: "GET"
+//     })
+//     request.success(function(response) {
+//       console.log(response);
+//       $scope.data = response.newRows;
+//     });
+//     request.error(function(err) {
+//       console.log('error: ' + err);
+//     });
+//   }
+// })
+
+// app.controller('postersController', function ($scope, $http) {
+//   console.log("inside posters controller");
+//   $scope.link = {}
+//   $scope.getPosters = function () {
+//     console.log('getting posters')
+//     var request = $http({
+//       url: "/randomIMDBIds",
+//       method: "GET"
+//     })
+//     request.success(function (response) {
+//       console.log(response);
+//       $scope.data = response.newRows;
+//     });
+//     request.error(function (err) {
+//       console.log('error: ' + err);
+//     });
+//   }
+//   $scope.getLinkFor = function (curr_id) {
+//     console.log(curr_id);
+//     var request = $http({
+//       url: 'http://www.omdbapi.com/?i=' + curr_id + '&apikey=d0656bab',
+//       method: "GET"
+//     })
+//     request.success(function (response) {
+//       $scope.link[curr_id] = response['Website'];
+//     });
+//     request.error(function (err) {
+//       console.log('error: ' + err);
+//     });
+//   }
+// })
+
+
+
+// Template for adding a controller
+/*
+app.controller('dummyController', function($scope, $http) {
+  // normal variables
+  var dummyVar1 = 'abc';
+
+  // Angular scope variables
+  $scope.dummyVar2 = 'abc';
+
+  // Angular function
+  $scope.dummyFunction = function() {
+
+  };
+});
+*/
---
+++ routes/index.js
@@ -0,0 +1,240 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+var User = require('../models/user.js')
+
+
+// // Connect string to MySQL
+// var mysql = require('mysql');
+
+// var connection = mysql.createConnection({
+//   host: 'fling.seas.upenn.edu',
+//   user: 'kecikde',
+//   password: 'Food&freizeitmoeglichkeiten',
+//   database: 'kecikde'
+// });
+
+// connection.connect(function (err) {
+//   if (err) {
+//     console.log("Error Connection to DB" + err);
+//     return;
+//   }
+//   console.log("Connection established...");
+// });
+
+// /* GET home page. */
+router.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'welcome.html'));
+});
+
+router.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'login.html'));
+});
+
+router.get('/dashboard', function (req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'dashboard.html'));
+});
+
+router.get('/register', function (req, res) {
+  res.sendFile(path.join(__dirname, '../', 'views', 'register.html'));
+});
+
+// router.get('/reference', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../', 'views', 'reference.html'));
+// });
+
+// router.get('/recommendations', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../', 'views', 'recommendations.html'));
+// });
+
+// router.get('/bestof', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../', 'views', 'bestof.html'));
+// });
+
+// router.get('/posters', function (req, res) {
+//   res.sendFile(path.join(__dirname, '../', 'views', 'posters.html'));
+// });
+
+// // To add a new page, use the templete below
+// /*
+// router.get('/routeName', function(req, res) {
+//   res.sendFile(path.join(__dirname, '../', 'views', 'fileName.html'));
+// });
+// */
+
+
+// Register uses POST request
+router.post('/register', function (req, res) {
+  console.log(req.body);
+  if (req.body.username && req.body.password) {
+    var { username, name, password } = req.body
+    var query = new User({ username, name, password })
+    query.save(function (err, result) {
+      if (err) next(err)
+      res.json({ status: 'OK' })
+    })
+  }
+})
+
+// // Login uses POST request
+router.post('/login', function (req, res) {
+  // use console.log() as print() in case you want to debug, example below:
+  // console.log(req.body); will show the print result in your terminal
+
+  // req.body contains the json data sent from the loginController
+  // e.g. to get username, use req.body.username
+  console.log(req.body);
+  if (req.body.username && req.body.password) {
+    var { username, password } = req.body
+    // var query = new User({ username, name, password })
+    User.find({ username, password }, function (err, results) {
+      if (!err) {
+        res.json({results, status: 'OK'})
+      } else {
+        next(err)
+      }
+    })
+  }
+})
+
+// // Dashboard uses GET request for users
+// router.get('/dashboardUsers', function (req, res) {
+//   var query = "SELECT DISTINCT username FROM User;";
+//   connection.query(query, function (err, rows, fields) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(rows);
+//       console.log(query);
+//       res.json({
+//         result: 'success',
+//         rows
+//       });
+//     }
+//   });
+// });
+
+// // Dashboard uses GET request for movies
+// router.get('/genres', function (req, res) {
+//   var query = "SELECT DISTINCT genre FROM Genres;";
+//   connection.query(query, function (err, rows, fields) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.json({
+//         result: 'success',
+//         rows
+//       })
+//     }
+//   })
+// })
+
+// // Dashboard uses GET request for movies
+// router.post('/genreTop10', function (req, res) {
+//   var query = "SELECT m.title, m.rating, m.vote_count from Movies m, Genres g WHERE m.id=g.movie_id AND g.genre='" + req.body.currentGenre + "'ORDER BY m.rating DESC, m.vote_count DESC LIMIT 10;";
+//   connection.query(query, function (err, rows, fields) {
+//     console.log(rows)
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.json({
+//         result: 'success',
+//         rows
+//       })
+//     }
+//   })
+// })
+
+// // Dashboard uses GET request for movies
+// router.post('/recommendationsurl', function (req, res) {
+//   var query = "SELECT DISTINCT m.title, g.genre FROM Movies m, Genres g WHERE g.movie_id=m.id AND g.genre IN (SELECT g.genre FROM Genres g, Movies m WHERE m.id=g.movie_id AND m.id=" + req.body.current_choice + ") AND m.id<> " + req.body.current_choice + " GROUP BY m.id LIMIT 10;"
+//   connection.query(query, function (err, rows, fields) {
+//     console.log(rows)
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.json({
+//         result: 'success',
+//         rows
+//       })
+//     }
+//   })
+// })
+
+// // Dashboard uses GET request for movies
+// router.post('/yearsandyears', function (req, res) {
+
+//   var query = "SELECT g.genre, m.title, MAX(m.vote_count) FROM Genres g, Movies m WHERE m.id=g.movie_id AND m.release_year=" + req.body.current_year + " GROUP BY g.genre DESC;";
+//   console.log(query);
+//   connection.query(query, function (err, rows, fields) {
+//     console.log(rows)
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.json({
+//         result: 'success',
+//         rows
+//       })
+//     }
+//   })
+// })
+
+// // // Dashboard uses GET request for movies
+// // router.get('/randomIMDBIds', function(req, res) {
+// //   //  var query = 'SELECT m.title from Movies m, Genres g WHERE m.id=g.movie_id ORDER BY m.rating DESC LIMIT 10;';
+// //   var query = "SELECT imdb_id, title FROM Movies ORDER BY rand() LIMIT 10;";
+// //   connection.query(query, function(err, rows, fields) {
+// //     if (err) {
+// //       console.log(err);
+// //     } else {
+// //       console.log(rows);
+// //       var newRows = rows.map(function(elem) {
+// //         return {img: ('http://img.omdbapi.com/?apikey=d0656bab&i=' + elem.imdb_id), title: elem.title} 
+// //       });
+// //       res.json({
+// //         result: 'success',
+// //         newRows
+// //       })
+// //     }
+// //   })
+// // })
+
+// // Dashboard uses GET request for movies
+// router.get('/randomIMDBIds', function (req, res) {
+//   var query = "SELECT imdb_id, title FROM Movies ORDER BY rand() LIMIT 10;";
+//   connection.query(query, function (err, rows, fields) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(rows);
+//       var newRows = rows.map(function (elem) {
+//         return { img: ('http://img.omdbapi.com/?apikey=d0656bab&i=' + elem.imdb_id), title: elem.title, id: elem.imdb_id }// link: } 
+//       });
+//       console.log(newRows);
+//       res.json({
+//         result: 'success',
+//         newRows
+//       })
+//     }
+//   })
+// })
+
+// // template for GET requests
+// /*
+// router.get('/routeName/:customParameter', function(req, res) {
+
+//   var myData = req.params.customParameter;    // if you have a custom parameter
+//   var query = '';
+
+//   // console.log(query);
+
+//   connection.query(query, function(err, rows, fields) {
+//     if (err) console.log(err);
+//     else {
+//       res.json(rows);
+//     }
+//   });
+// });
+// */
+
+module.exports = router;
